f78a2ebde331a227f6f4fc4eb8e6be19b0433bad
Dominic White <dewhite4@gmail.com>
2018-09-12 07:59:59 -0400

    Fix: Switch to aria-label
    - switch from title to aria-label to makes links accessible

--- js/main.js
+++ js/main.js
@@ -179,7 +179,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  more.title = "More details about " + restaurant.name;
+  more.setAttribute("aria-label", "More details about " + restaurant.name);
   li.append(more)
 
   return li
