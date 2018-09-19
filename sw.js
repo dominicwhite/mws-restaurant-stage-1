eaef5ecb947e3e02871a508994a493dff318fd9a
Dominic White <dewhite4@gmail.com>
2018-09-18 21:59:46 -0400

    Feat: Cache new requests
    Service worker fetch handler updated to cache requests for new
    content not included in the initial URLs.

--- sw.js
+++ sw.js
@@ -16,7 +16,21 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then( function(response){
       if (response) return response;
-      return fetch(event.request);
+      const fetchRequest = event.request.clone();
+      return fetch(fetchRequest).then(
+        function(response) {
+          if (!response || response.status != 200 || response.type != 'basic') {
+            return response;
+          }
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME).then(
+            function(cache){
+              cache.put(event.request, responseToCache);
+            }
+          );
+          return response;
+        }
+      );
     })
   );
 });
